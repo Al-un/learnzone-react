c9c1ca83e1adb53c616f5fcd6d2f6cc7f181d03b
Al-un <alun.sng@gmail.com>
2018-12-04 17:23:56 +0100

    Adding audience

--- src/services/auth/auth0_config.js
+++ src/services/auth/auth0_config.js
@@ -1,14 +1,20 @@
+const base_url =
+  process.env.BASE_URL || "https://al-un.github.io/learnzone-react";
+
 export const AUTH0_CONFIG =
   process.env.NODE_ENV === "development"
     ? {
         domain: "al-un-dev.eu.auth0.com",
         clientID: "LcFOtvd8rXXxeC9XFnAYD3Ysypq2tpRE",
         redirectUri: "http://localhost:3000/callback",
-        logoutUrl: "http://localhost:3000"
+        logoutUrl: "http://localhost:3000",
+        audience: "https://rails.learnzone.com"
+        // audience: "https://node.learnzone.com"
       }
     : {
         domain: "al-un.eu.auth0.com",
         clientID: "",
-        redirectUri: "https://al-un.github.io/learnzone-react/callback",
-        logoutUrl: "https://al-un.github.io/learnzone-react/"
+        redirectUri: `${base_url}/callback`,
+        logoutUrl: `${base_url}/`,
+        audience: "https://rails.learnzone.com"
       };
