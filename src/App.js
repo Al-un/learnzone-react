091a2d4f618c1d86d6828cfdc83698b58c2bfa1d
Al-un <alun.sng@gmail.com>
2018-12-13 18:06:04 +0100

    router basename

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 //React
 import React, { Component } from "react";
 import { Provider } from "react-redux";
-import { Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 // Styles
 import "./stylesheets/application.scss";
 // Services
@@ -13,11 +13,14 @@ import Layout from "./layout/Layout";
 
 class App extends Component {
   render() {
+    const basename =
+      process.env.NODE_ENV === "development" ? "/" : "/learnzone";
+
     return (
       <Provider store={store}>
-        <Router history={history}>
+        <BrowserRouter history={history} basename={basename}>
           <Layout auth={auth} />
-        </Router>
+        </BrowserRouter>
       </Provider>
     );
   }
